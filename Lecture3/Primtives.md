1b5476e8f1a9f69df16b373e89d2370005479594
muhammadmaaz2k5 <22pwbcs0933@uetpeshawar.edu.pk>
2026-07-06 05:09:53 +0500

    added the primitive lecture 3

---
+++ Lecture3/Primtives.md
@@ -0,0 +1,12 @@
+on the javascript we have two categories of types:
+1. Primitive types
+2. Reference types
+
+Primitive:
+1. String
+2. Number
+3. Boolean
+4. null
+5. undefined
+6. BinInt
+7. Symbol
---
+++ Lecture3/primitives.js
@@ -0,0 +1,45 @@
+// String
+
+let name = 'Muhammad Maaz'
+console.log(name);
+
+// Number
+
+const age = 20
+if (age >= 18) {
+    console.log("You are eligible for voting");
+}
+else {
+    console.log("You are not eligible for voting");
+}
+
+// Boolean
+
+const isAdmin = true;
+if (isAdmin) {
+    console.log("welcome to dashboard")
+} else {
+    console.log("you are not a admin")
+}
+
+// Null
+
+let profile = null;
+console.log(typeof profile)
+
+// undefined
+
+let number = undefined;
+console.log(typeof number)
+
+// typeof use for checking the type of the variable
+
+// BigInt
+
+const number1 = 123456789012345678901234567890n
+console.log(typeof number1)
+
+// Symbol
+
+const id = Symbol('123')
+console.log(typeof id)
